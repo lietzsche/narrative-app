5c7110b93e2d2cfb0d117dda5bd8ab63bd28e217
bion0625 <bion0625@gmail.com>
2025-08-15 17:59:47 +0900

    github page 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { initContent } from './initContent';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
             <App />
         </BrowserRouter>
     </React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
     plugins: [react()],
-    base: '/narrative-app/',
-});
+    // dev: '/', build: '/narrative-app/'
+    base: command === 'build' ? '/narrative-app/' : '/',
+}))
